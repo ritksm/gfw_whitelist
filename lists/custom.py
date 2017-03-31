6b2215d3a2136b85093b63d65926c188c47afff9
Jack Rivers <ritksm@gmail.com>
2017-03-31 22:31:27 +0800

    update custom

--- lists/custom.py
+++ lists/custom.py
@@ -3,5 +3,14 @@
 
 def getlist():
     liststr = """
+qiniucdn.com
+wangyin.com
+gewara.com
+ele.me
+eleme.io
+elenet.me
+rajax.me
+teambition.com
+anquanbao.com
 """
     return set(liststr.splitlines(False))
